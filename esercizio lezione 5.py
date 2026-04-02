1559b1f797708cf3018abee510e991504c85d6c4
ariellecharonne <ariellecharonne237@gmail.com>
2026-04-02 20:21:39 +0200

    esercizio lezione 5

---
+++ esercizio lezione 5.py
@@ -0,0 +1,57 @@
+ # esercizio 1
+
+class canguro:
+    def __init__(self,tasca):
+        self.tasca = []
+        def intasca(self,x):
+            self.tasca.appened(x)
+            def _str_(self):
+                return str(self.tasca)
+
+                #esercizio 2
+
+            # esercizio2
+            class veicolo ():
+                def _init_(self,marca,modello)
+                self.marca = marca 
+                self.modello = modello
+                def _str_(self):
+                    return veicolo "{}""{}".format(self.marca,self.modulo)
+
+                    classmoto(veicolo):
+                    def _init_(self,marca,modello)
+                    self.marca = marca
+                    self.modello = modello
+                    super() self._init_(marca,modello)
+                    self.modello = modello
+                    self.tipo = tipo
+
+                    class auto (veicolo):
+                        def _init_(self, marca, modello):
+                            self.marca = marca 
+                            self.modello = modelllo
+                            super() _init_ (self.marca,self.modello)
+                            return self.marca + ",,"self.modello + "porte:"
+
+
+                            #esercizio 3
+                            classpersona():
+                            def _init_(self,ruolo,nome,cognome):
+                                self.ruolo = ruolo 
+                                self.nome = nome 
+                                self.cognome = cognome 
+
+                                classstudente(persona):
+                                def _init_(self,nome,cognome,corsi:
+                                    super() _init_("docente,nome,cognome")
+                                    self.corsi = corsi 
+
+                                
+
+                
+_
+
+
+
+
+        
