5ef23cebb1286d383f8d4b4acfc20efcfbfcc4a4
ariellecharonne <ariellecharonne237@gmail.com>
2026-04-02 19:35:38 +0200

    esercizio lezione 4

---
+++ esercizio lezione4 .py
@@ -0,0 +1,27 @@
+class veicolo():
+    def _init_(self,modello,marca,anno):
+        self.modello = modello
+        self.marca = marca 
+        self.anno = anno
+        self.speed = o
+        def _str_(self):
+            return veicolo ."{}{}.format (self.modello,self.marca,self.anno,self.speed)"
+            def accellerare(self):
+                self.speed = selfspeed +5
+                def frenare(self):
+                    selfspeed = selfspeed - 5
+                    def getspeed(self):
+         return self.speed 
+
+
+# esecizio 2
+
+class cvsFile()
+def _init_(self,name):
+    self.name = name
+    def get_data(self):
+        file = open(self.name)
+        data = []
+        for line in file data . append(line.strip().split(","))
+        return data
+        
